17e8307a07c575af1ab52b6ea8bd28d3401f90bc
Cosebdd <cosebdd@gmail.com>
2016-06-05 02:47:48 +0300

    modified:   Stickie/App.xaml 	new file:   Stickie/Controls/TitleTextBox.cs 	modified:   Stickie/MainWindow.xaml 	modified:   Stickie/MainWindow.xaml.cs 	modified:   Stickie/Stickie.csproj 	deleted:    Stickie/TitleTextBox.cs 	modified:   Stickie/UnmaximizibleWindow.cs 	deleted:    Stickie/User32KeyboardListener.cs 	deleted:    Stickie/User32WindowHandler.cs 	modified:   Stickie/WindowBehaviorAppliers/WindowNoMaximizeOnAeroSnapApplier.cs 	Stickie/Styles/ 	Stickie/WinLibs/

--- Stickie/MainWindow.xaml.cs
+++ Stickie/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
@@ -11,49 +10,21 @@ namespace Stickie
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow : Window
     {
-        private IntPtr _hwnd;
-
-        private Point _startPoint;
-
         public MainWindow()
         {
-            InitializeComponent();
-            var viewModel = new StickerViewModel();
-            DataContext = viewModel;
-            _startPoint = new Point();
-
             List<IWindowBehaviorApplier> BehaviorList = new List<IWindowBehaviorApplier>();
             BehaviorList.Add(new WindowNoMaximizeOnAeroSnapApplier());
             BehaviorList.Add(new WindowOpacityApplier());
             BehaviorList.Add(new WindowClickThroughApplier());
             foreach (IWindowBehaviorApplier Behavior in BehaviorList)
                 Behavior.ApplyBehavior(this);
+
+            InitializeComponent();
+            var viewModel = new StickerViewModel();
+            DataContext = viewModel;
         }
 
-
-
-        private void TitleBox_OnPreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            var control = (IInputElement) sender;
-            var currentPoint = e.GetPosition(control);
-            if (e.LeftButton == MouseButtonState.Pressed &&
-                control.IsMouseCaptured &&
-                (Math.Abs(currentPoint.X - _startPoint.X) >
-                 SystemParameters.MinimumHorizontalDragDistance ||
-                 Math.Abs(currentPoint.Y - _startPoint.Y) >
-                 SystemParameters.MinimumVerticalDragDistance))
-            {
-                // Prevent Click from firing
-                control.ReleaseMouseCapture();
-                DragMove();
-            }
-        }
-
-        private void TitleBox_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            _startPoint = e.GetPosition((IInputElement)sender);
-        }
     }
 }
