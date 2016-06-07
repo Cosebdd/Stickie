c286135d7d971a73a08932655bc077ee68b10bbc
Cosebdd <cosebdd@gmail.com>
2016-06-08 00:49:24 +0300

    Unnecessary usings removed

--- Stickie/MainWindow.xaml.cs
+++ Stickie/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Collections.Generic;
+﻿using System.Windows;
 
 namespace Stickie
 {
--- Stickie/StickerViewModel.cs
+++ Stickie/StickerViewModel.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using Stickie.Annotations;
 
 namespace Stickie
@@ -18,8 +13,7 @@ namespace Stickie
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            var handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         public string Title
